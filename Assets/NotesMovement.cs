17f365fa9905798a2e67d14c59f321170b8aa010
MSI\robert <robertjdoig@hotmail.co.uk>
2017-05-19 23:27:53 +0100

    Adding all the music controller and note prefabs. will add sounds and line renderer to ui

---
+++ Assets/NotesMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesMovement : MonoBehaviour {
+
+    public RectTransform trans;
+
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+        trans = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        trans.anchoredPosition = new Vector2(trans.anchoredPosition.x - speed, 0);
+	}
+}
