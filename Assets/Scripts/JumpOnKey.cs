da5b3d05a9233520ac0b5390c0eb76dec0d8e0d4
MSI\robert <robertjdoig@hotmail.co.uk>
2017-05-20 15:08:44 +0100

    adding the hermit curve to the jump

--- Assets/Scripts/JumpOnKey.cs
+++ Assets/Scripts/JumpOnKey.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class JumpOnKey : MonoBehaviour {
 
-    public GameObject currentWp;
-    public GameObject currentKey; 
 
+    public float speed = 1.0f;
+    private float startTime;
+    private float journeyLength;
+
+    [Range(1,30)]
+    public float ang_scl = 1;
+
+    public GameObject currentWp;
+    public GameObject currentKey;
+
+    [SerializeField]
+    private bool isJumping = false;
 
     public Rigidbody rb;
     public PlayerController pc;
-
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
         pc = GetComponent<PlayerController>();
+
+        currentWp = pc.SelectedKey.Waypoint.gameObject;
+        currentKey = pc.SelectedKey.BouncingPad;
+
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(currentWp.transform.position, currentKey.transform.position);
     }
 	
 	// Update is called once per frame
@@ -23,13 +38,47 @@ public class JumpOnKey : MonoBehaviour {
         currentWp = pc.SelectedKey.Waypoint.gameObject;
         currentKey = pc.SelectedKey.BouncingPad;
 
-        if (Input.GetButtonDown("Jump")){
-            rb.MovePosition(currentKey.transform.position);
+
+
+        //transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+        if(isJumping == false)
+        {
+            if (Input.GetButtonDown("Jump")){
+                isJumping = true;
+                startTime = Time.time;
+            }
+
         }
 
-        if (Input.GetButtonUp("Jump"))
+        if(isJumping == true)
         {
-            rb.MovePosition(currentWp.transform.position);
+            float distCovered = (Time.time - startTime) * speed;
+            rb.MovePosition(hermit(currentWp.transform.position, currentKey.transform.position, new Vector3(0,ang_scl,0), new Vector3(0,ang_scl,0), distCovered));
+            if(distCovered >= 1)
+            {
+                rb.MovePosition(currentWp.transform.position);
+                isJumping = false;
+            }
         }
-	}
+
+        //rb.MovePosition(Vector3.Lerp(startMarker.position, endMarker.position, fracJourney));
+    }
+
+    static public Vector3 hermit(Vector3 p0, Vector3 p1, Vector3 t0, Vector3 t1, float t)
+    {
+        float[] coeff = new float[4];
+        coeff[0] = 1 - (3 * t * t) + (2 * t * t * t);
+        coeff[1] = t * t * (3 - 2 * t);
+        coeff[2] = t * (1 - 2 * t + t * t);
+        coeff[3] = t * t * (1 - t);
+
+        // More Efficient then for loop
+        Vector3 result = Vector3.zero;
+        result += coeff[0] * p0;
+        result += coeff[1] * p1;
+        result += coeff[2] * t0;
+        result += coeff[3] * t1;
+
+        return result;
+    }
 }
