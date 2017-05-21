759bc3598ba596112ca42a555ae2dbf2d6de6bd5
MSI\robert <robertjdoig@hotmail.co.uk>
2017-05-21 16:54:04 +0100

    Adding random colors when the player spawns in the game when pressing A

--- Assets/Scripts/joyController.cs
+++ Assets/Scripts/joyController.cs
@@ -13,12 +13,26 @@ public class joyController : MonoBehaviour {
 
     public bool[] isUsed = new bool[11];
 
+    Color[] colors = new Color[10];
+
+    
 
 
     public PianoKey key;
 	// Use this for initialization
 	void Start () {
         spawnLocation = gameObject.transform.GetChild(0).position;
+
+        colors[0] = Color.red;
+        colors[1] = Color.yellow;
+        colors[2] = Color.magenta;
+        colors[3] = new Color(252, 88,0);
+        colors[4] = Color.cyan;
+        colors[5] = Color.blue;
+        colors[6] = Color.green;
+        colors[7] = new Color(247, 165, 24);
+        colors[8] = new Color(196, 62, 178);
+        colors[9] = new Color(122, 0, 252);
 
 
 	}
@@ -34,7 +48,7 @@ public class joyController : MonoBehaviour {
                     isUsed[i-1] = true;
                     print("JoyA" + i + "  its this one dummy" + "   Joy " + i);
                     GameObject newPlayer = Instantiate(playerPrefab, spawnLocation, Quaternion.identity);
-                    newPlayer.GetComponent<PlayerController>().setupCharacter("JoyA" + i, "Joy" + i, Color.blue, key);
+                    newPlayer.GetComponent<PlayerController>().setupCharacter("JoyA" + i, "Joy" + i,colors[(int)Random.Range(0,9)] , key);
                     break;
                 }
                 else
