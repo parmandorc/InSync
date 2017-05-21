68334d817aa4ce972821a7a92563cbe4b09f542a
Pablo Armando Rodríguez Codes <parmandorc@gmail.com>
2017-05-21 17:56:47 +0200

    Added keyboard players

--- Assets/Scripts/joyController.cs
+++ Assets/Scripts/joyController.cs
@@ -4,49 +4,48 @@ using UnityEngine;
 
 public class joyController : MonoBehaviour {
 
-    public Vector3 spawnLocation;
-    public PlayerController player1;
+    [SerializeField]
+    private GameObject SpawnLocation;
 
-    public PlayerController player2;
+    [SerializeField]
+    private PianoKey DefaultKey;
 
-    public GameObject playerPrefab;
+    [SerializeField]
+    private GameObject playerPrefab;
 
-    public bool[] isUsed = new bool[11];
+    private bool[] isUsed = new bool[11];
 
-
-
-    public PianoKey key;
-	// Use this for initialization
-	void Start () {
-        spawnLocation = gameObject.transform.GetChild(0).position;
-
-
-	}
+    private bool isKeyboardUsed1, isKeyboardUsed2;
 
     // Update is called once per frame
     void Update() {
 
-        
+        if (!isKeyboardUsed1 && Input.GetButtonDown("Jump1"))
+        {
+            isKeyboardUsed1 = true;
+            SpawnPlayer("Jump1", "Horizontal1", Color.blue, DefaultKey);
+        }
+
+        if (!isKeyboardUsed2 && Input.GetButtonDown("Jump2"))
+        {
+            isKeyboardUsed2 = true;
+            SpawnPlayer("Jump2", "Horizontal2", Color.blue, DefaultKey);
+        }
+
         for (int i = 1; i < 12; i++)
         {
             if (Input.GetButtonUp("JoyA" + i) && isUsed[i - 1] == false)  
             {
-                    isUsed[i-1] = true;
-                    print("JoyA" + i + "  its this one dummy" + "   Joy " + i);
-                    GameObject newPlayer = Instantiate(playerPrefab, spawnLocation, Quaternion.identity);
-                    newPlayer.GetComponent<PlayerController>().setupCharacter("JoyA" + i, "Joy" + i, Color.blue, key);
-                    break;
-                }
-                else
-                {
-                    print("i am working");
-                }
+                isUsed[i - 1] = true;
+                SpawnPlayer("JoyA" + i, "Joy" + i, Color.blue, DefaultKey);
+                break;
             }
-
         }
-
-
-
     }
 
-    
+    void SpawnPlayer(string enterButton, string moveAxis, Color colorKey, PianoKey key)
+    {
+        GameObject newPlayer = Instantiate(playerPrefab, SpawnLocation.transform.position, Quaternion.identity);
+        newPlayer.GetComponent<PlayerController>().setupCharacter(enterButton, moveAxis, colorKey, key);
+    }
+}
